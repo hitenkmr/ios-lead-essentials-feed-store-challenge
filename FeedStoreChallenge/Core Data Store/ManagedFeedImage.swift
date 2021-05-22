287ae5e4a3bd8aeb913050304b3a5009f15274ed
Hitender Kumar <hitender.hk@gmail.com>
2021-05-22 12:51:13 +0530

    Create 'ManagedFeedImage' entity in core data model.

---
+++ FeedStoreChallenge/Core Data Store/ManagedFeedImage.swift
@@ -0,0 +1,17 @@
+//
+//  ManagedFeedImage.swift
+//  FeedStoreChallenge
+//
+//  Created by Hitender Kumar on 22/05/21.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import CoreData
+
+@objc(ManagedFeedImage)
+internal class ManagedFeedImage: NSManagedObject {
+	@NSManaged internal var id: UUID
+	@NSManaged internal var imageDescription: String?
+	@NSManaged internal var location: String?
+	@NSManaged internal var url: URL
+}
