d50e7d5c6447e104e36c2b7ee88860d6ddf980ea
Hitender Kumar <hitender.hk@gmail.com>
2021-05-22 13:38:54 +0530

    Implement retrieve and insert functions and ensure that retrieve delivers values on non empty cache.

--- FeedStoreChallenge/Core Data Store/ManagedFeedCache.swift
+++ FeedStoreChallenge/Core Data Store/ManagedFeedCache.swift
@@ -9,8 +9,25 @@
 import Foundation
 import CoreData
 
-@objc(FeedCache)
+@objc(ManagedFeedCache)
 internal class ManagedFeedCache: NSManagedObject {
 	@NSManaged internal var timestamp: Date
 	@NSManaged internal var feed: NSOrderedSet
+}
+
+extension ManagedFeedCache {
+	internal static func find(in context: NSManagedObjectContext) throws -> ManagedFeedCache? {
+		let request = NSFetchRequest<ManagedFeedCache>(entityName: entity().name!)
+		request.returnsObjectsAsFaults = false
+		return try context.fetch(request).first
+	}
+
+	internal static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedFeedCache {
+		try find(in: context).map(context.delete)
+		return ManagedFeedCache(context: context)
+	}
+
+	internal var localFeed: [LocalFeedImage] {
+		return feed.compactMap { ($0 as? ManagedFeedImage)?.local }
+	}
 }
